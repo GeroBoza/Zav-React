74827ef644b4573d366e434016ae2d5d42c78d9d
Geronimo Boza <geronimoboza@gmail.com>
2020-11-05 17:19:00 -0300

    styles editados

--- src/components/Products/ProductDescription.js
+++ src/components/Products/ProductDescription.js
@@ -1,37 +1,51 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 const ProductDescription = (props) => {
+    const { prod } = props;
 
-    const {prod} = props
-
-//    { prod !== undefined ? console.log("Hola") : console.log("fuck")}
-//     return
+    //    { prod !== undefined ? console.log("Hola") : console.log("fuck")}
+    //     return
     return (
-        <article className="item">                      
-                                
-                             <h2>{prod.brand.name} </h2>
+        <article className="item">
+            <h2>{prod.brand.name} </h2>
 
-                            {prod.subcategoryId !== 2 ?
-                            <Fragment>
-                            <p className="mb-0">Artículo: {prod.article} </p> 
-                            <p className="mb-0" >Color: {prod.color}</p>
-                            <p className="mb-0">Numeración: {prod.numeration}</p>
-                            <p className="mb-0">{prod.article + " " + prod.color}</p>
-                            <p className="mt-5 mx-auto" style={{width:"80%", fontSize:"20px"}}>Descripción: { prod.description}</p>
-                            </Fragment>
-                            :  "" }
-                            
-                            
-                            {prod.stock !== 0 ? 
-                            <h3 style={{marginBottom : "3%"}}>Precio: ${ prod.price}</h3>
-                            : 
-                            <Fragment>
-                                <p>Precio: $ {prod.price}</p>
-                                <h3 style={{marginBottom : "3%"}}>SIN STOCK</h3>
-                            </Fragment>}
-                               
-                                
-                            {/*
+            {prod.subcategoryId !== 2 ? (
+                <Fragment>
+                    <p className="mb-0">Artículo: {prod.article} </p>
+                    <p className="mb-0">Color: {prod.color}</p>
+                    <p className="mb-0">Numeración: {prod.numeration}</p>
+                    <p className="mb-0">{prod.article + " " + prod.color}</p>
+                    <p
+                        className="mt-5 mx-auto"
+                        style={{ width: "80%", fontSize: "20px" }}
+                    >
+                        Descripción: {prod.description}
+                    </p>
+                </Fragment>
+            ) : (
+                ""
+            )}
+
+            {prod.stock !== 0 ? (
+                <h3 style={{ marginBottom: "3%" }}>Precio: ${prod.price}</h3>
+            ) : (
+                <Fragment>
+                    <p>Precio: $ {prod.price}</p>
+                    <h3 style={{ marginBottom: "3%" }}>SIN STOCK</h3>
+                </Fragment>
+            )}
+
+            <a
+                id="wsp-btn"
+                href={`https://web.whatsapp.com/send?phone=542216395806&text=Hola, quería consultar por este producto: https://www.zapateriaavenida.com/detalleProducto/${prod.id}`}
+                target="_blank"
+                className="nav-link wsp"
+                rel="noreferrer"
+            >
+                <button style={{fontSize:"20px", padding:"2px 15px"}}>Consultar por este producto</button>
+            </a>
+
+            {/*
              
                                         
                                         <% if (user && user.rol == 2) {%>
@@ -67,9 +81,8 @@ const ProductDescription = (props) => {
                                                         </a>
                                                         
                                                     <% } %> */}
-                            
-                        </article>
-    )
-}
+        </article>
+    );
+};
 
-export default ProductDescription
+export default ProductDescription;
